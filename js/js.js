b9e78e10dac696728c77c871751298ed412a4985
Simon Newman <si.newman87@gmail.com>
2016-11-21 20:56:28 +0000

    Initial release

---
+++ js/js.js
@@ -0,0 +1,45 @@
+function resize(width, height, className){
+  $(className).css("min-height", ($(className).width()*(height/width)));
+}
+
+function resizeAll() {
+  resize(1920, 981, ".slider");
+  resize(1270, 420, ".interior");
+}
+
+$(window).resize(function(){
+  resizeAll();
+});
+
+$(function(){
+  resizeAll();
+});
+
+$(".featuresPicture .circle").hover(function(){
+  $(this).parent().find(".content").fadeIn();
+},function(){
+  $(this).parent().find(".content").fadeOut();
+});
+
+var intervalID = window.setInterval(sliderTimer, 4000);
+
+function sliderTimer() {
+	var images = ["images/bg4.jpg", "images/bg2.jpg"];
+
+	nextImage = $(".slider").data("next-bg");
+	console.log(images.length);
+
+	if (nextImage) {
+		if (nextImage == (images.length - 1)) { // last image
+			nextImage = 0;
+		} else {
+			nextImage++;
+		}
+		$(".slider").data("next-bg", nextImage);
+		$(".slider").css("background-image", "url(" + images[nextImage] + ")");
+	} else {
+		nextImage = 1;
+		$(".slider").data("next-bg", nextImage);
+		$(".slider").css("background-image", "url(" + images[nextImage] + ")");
+	}
+}
